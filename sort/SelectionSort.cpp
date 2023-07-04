ed97513f11bb7a8c151b04635933b023defc22fe
Kapil Kaushik <kapil.kaushik@cavisson.com>
2023-07-04 18:03:41 +0530

    code added for selection sort

--- sort/SelectionSort.cpp
+++ sort/SelectionSort.cpp
@@ -1,13 +1,29 @@
 #include "iostream"
+#include "vector"
 
 using namespace std;
 
-void print(int arr[], int n) {
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+void print(vector<int> v) {
+    for (int i: v) cout << i << " ";
     cout << endl;
 }
 
+void selectionSort(vector<int> &v){
+for(int i=0;i<v.size();i++){
+    int m = i;
+    for(int j=i+1;j<v.size();j++){
+        if(v[m]>v[j]) m = j;
+    }
+    swap(v[i],v[m]);
+}
+}
+
 int main(){
+
+    vector<int> v = {1,4,12,8,0,33,23};
+    print(v);
+    selectionSort(v);
+    print(v);
 
 return 0;
 }
